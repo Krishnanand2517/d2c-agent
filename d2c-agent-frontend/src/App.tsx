38d5e44c65c77f1738064f392915f55823d97d85
Krishnanand Yadav <krishnanand2517@gmail.com>
2026-05-11 18:04:18 +0530

    Initial commit

---
+++ d2c-agent-frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import "./index.css";
+
+export function App() {
+  return (
+    <div className="max-w-7xl mx-auto p-8 text-center relative z-10">
+      <h1>Hey</h1>
+    </div>
+  );
+}
+
+export default App;
